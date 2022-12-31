cfc28d9b5c13365a71bdc724313c26d2474af54c
parmarrudr4 <118604670+parmarrudr4@users.noreply.github.com>
2022-12-31 18:45:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/Terabox/TeraboxSearchBotV2.git /TeraboxSearchBotV2
+cd /TeraboxSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
